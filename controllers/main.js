84d727b3fff11cbc9af1519fe12189cf558d2e91
ahmed7761 <sharif7761@gmail.com>
2023-08-25 20:27:19 +0600

    generate jwt token

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,16 @@
-
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
-    res.send('Fake Login/Register/Signup Route')
+    const { username, password } = req.body
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+    // demo id
+    const id = new Date().getDate()
+    const token = jwt.sign({id, username},process.env.JWT_SECRET, {expiresIn: '30d'})
+    res.status(200).json({msg: `User created`, token})
 }
 
 const dashboard = async (req, res) => {
