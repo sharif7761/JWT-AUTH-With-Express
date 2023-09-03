2e1c362f097e7cfdf0d92d33f4ed36156e31ced0
ahmed7761 <sharif7761@gmail.com>
2023-09-04 00:06:14 +0600

    add auth middleware

--- controllers/main.js
+++ controllers/main.js
@@ -15,18 +15,9 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomAPIError('No token provided', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-    try{
-       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        return res.status(200).json({msg: `Hello ${decoded.username}`, secret: `token is: ${token}`})
-    } catch (err){
-        throw new CustomAPIError('Not authorized to access this route', 401)
-    }
+    console.log(req.user)
+    const luckyNumber = Math.floor(Math.random() * 100)
+    return res.status(200).json({msg: `Hello ${req.user.username}`, secret: `Here is authorized data. your lucky number is: ${luckyNumber}`})
 }
 
 module.exports = {
